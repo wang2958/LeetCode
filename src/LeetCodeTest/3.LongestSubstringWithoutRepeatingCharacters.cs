17787e4e03e4cdeda456570e6d7d0fc369e9a077
杰丶Sue <1983273148@qq.com>
2025-07-26 22:25:37 +0800

    init 3

---
+++ src/LeetCodeTest/3.LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,54 @@
+﻿namespace LeetCodeTest
+{
+    public class _3_LongestSubstringWithoutRepeatingCharacters
+    {
+        /// <summary>
+        /// Input: s = "abcabcbb"
+        /// Output: 3
+        /// </summary>
+        [Fact]
+        public void Test1()
+        {
+            var s = "abcabcbb";
+            var expected = 3;
+            var result = LengthOfLongestSubstring(s);
+            Assert.Equal(expected, result);
+        }
+
+        /// <summary>
+        /// Input: s = "bbbbb"
+        /// Output: 1
+        /// </summary>
+        [Fact]
+        public void Test2()
+        {
+            var s = "bbbbb";
+            var expected = 1;
+            var result = LengthOfLongestSubstring(s);
+            Assert.Equal(expected, result);
+        }
+
+        /// <summary>
+        /// Input: s = "pwwkew"
+        /// Output: 3
+        /// </summary>
+        [Fact]
+        public void Test3()
+        {
+            var s = "pwwkew";
+            var expected = 3;
+            var result = LengthOfLongestSubstring(s);
+            Assert.Equal(expected, result);
+        }
+
+        /// <summary>
+        /// 给定一个字符串 s ，请你找出其中不含有重复字符的 最长 子串 的长度。
+        /// Given a string s, find the length of the longest substring without duplicate characters.
+        /// </summary> 
+        /// <returns></returns> 
+        public int LengthOfLongestSubstring(string s)
+        {
+
+        }
+    }
+}
